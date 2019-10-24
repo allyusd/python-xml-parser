f4ce7e4b036d10040b566712b7d7a6976c54ec4d
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2019-10-24 14:46:29 +0800

    feat: write quick_menu.xml

--- python-xml-parser.py
+++ python-xml-parser.py
@@ -1,7 +1,17 @@
 import xml.etree.ElementTree as ET
 
+from xml.dom import minidom
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ET.tostring(elem, encoding='unicode')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
+
 food_list = []
 
+# Read
 tree = ET.parse('xml/simple.xml')
 menu = tree.getroot()
 
@@ -13,3 +23,16 @@ for food in menu:
         food_list[food_index][data.tag] = data.text
 
 print(food_list)
+
+# Write
+quick_menu = ET.Element('quick_menu')
+
+for food in food_list:
+    item = ET.SubElement(quick_menu, 'item')
+    for data in food:
+        if data != 'description':
+            new_data = ET.SubElement(item, data)
+            new_data.text = food[data]
+
+with open("quick_menu.xml", "w") as text_file:
+    print(prettify(quick_menu), file=text_file)
